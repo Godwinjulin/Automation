cb2102fd8e1c85dea5a1da0e711032f1bcc43775
Nick <godwinjulin@gmail.com>
2022-09-13 13:31:11 +0100

    update

--- efs.tf
+++ efs.tf
@@ -9,7 +9,7 @@ resource "aws_kms_key" "ACS-kms" {
     {
       "Sid": "Enable IAM User Permissions",
       "Effect": "Allow",
-      "Principal": { "AWS": "arn:aws:iam::${var.account_no}:user/Nicholas" },
+      "Principal": { "AWS": "arn:aws:iam::${var.account_no}:user/terra" },
       "Action": "kms:*",
       "Resource": "*"
     }
