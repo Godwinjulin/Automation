dc4e06d6ed764ef24154b7183b02eb57179efb3c
Nick <godwinjulin@gmail.com>
2022-09-22 17:19:57 +0100

    fixed bug

--- efs.tf
+++ efs.tf
@@ -1,15 +1,16 @@
 # create key from key management system
+# create key from key management system
 resource "aws_kms_key" "ACS-kms" {
   description = "KMS key "
   policy      = <<EOF
-{
+  {
   "Version": "2012-10-17",
   "Id": "kms-key-policy",
   "Statement": [
     {
       "Sid": "Enable IAM User Permissions",
       "Effect": "Allow",
-      "Principal": { "AWS": "arn:aws:iam::348254368030:user/Tarra" },
+      "Principal": { "AWS": "arn:aws:iam::${var.account_no}:user/Terra" },
       "Action": "kms:*",
       "Resource": "*"
     }
